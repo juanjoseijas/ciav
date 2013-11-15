b0a6c8ba22eb9580089679d49b8074a432272835
Juanjo <juanjo@efimera.com>
2013-11-15 11:24:16 +0100

    Adds general info about the Tower; fixes juanjoseixas/ciav#5

---
+++ source/content/es/info.markdown
@@ -0,0 +1,47 @@
+**Horarios de la Torre**
+
+* Lunes-domingo
+* Octubre-mayo: 10.00-18.00h.
+* Junio-septiembre: 10.00-21.00h.
+
+**Horario del Centro**
+
+* Lunes-viernes: 09:00-20:30h
+* Sábados: 10.00-14.00h/16.00-20.30h
+* Domingos/Festivos: 10-15:30h
+
+**Líneas de autobús**
+
+* 3, 3A, 5
+
+**Localización GPS**
+
+* 43º 23 ́ N - 8º 24 ́ W
+
+[Ayuntamiento de A Coruña](http://www.coruna.es)
+
+* 981184200
+
+[Información turística](http://www.turismocoruna.com)
+
+* 981923093
+* [Coruña se mueve](http://www.corunasemueve.com)
+
+**Información ciudadana**
+
+* 010
+* 981184278
+
+[Autobuses](http://www.tranviascoruna.com)
+
+* 981 250 100
+
+[Aplicación para móviles Bus Coruña](http://www.buscoruna.eu)
+
+[Radio Taxi Coruña](http://www.radiotaxicoruna.es)
+
+* 981243333
+
+[Taxi Coruña](http://www.taxicoruna.es)
+
+* 981107066
---
+++ source/javascripts/map.js
@@ -0,0 +1,167 @@
+// Google Maps
+
+
+//var infowindow = null;
+    $(document).ready(function () { initialize();  });
+
+//function initialize() {
+
+var sources = [  
+
+['Marker 11', 43.386195, -8.406510, 3,'http://ciav.s3.amazonaws.com/icon/lighthouse-2.png','','<div>Torre de Hércules<div>']
+
+
+
+
+
+];
+
+var hercules = new google.maps.LatLng(43.385833, -8.406389);
+var MY_MAPTYPE_ID = 'custom_style';
+
+var featureOpts = [
+          
+        {
+          "featureType": "landscape.man_made",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },
+          {
+          "featureType": "landscape.natural",
+          "stylers": [
+            
+            { "color": "#E2E0DC" }
+          ]
+        },
+        {
+          "featureType": "water",
+          "stylers": [
+           
+            { "color": "#F8F7F6" }
+          ]
+        },
+        {
+          "featureType": "landscape",
+          "elementType": "labels",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },{
+          "featureType": "road",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },{
+          "featureType": "poi",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },
+        {
+            "featureType": "administrative",
+            "stylers": [
+            { "visibility": "off" }
+            ]
+        }
+        ];
+
+
+
+var lines = [];
+
+   
+    
+
+          
+// Creating an object literal containing the properties we want to pass to the map 
+          var mapOptions = {
+          zoom: 5,
+          center: hercules,
+          mapTypeControlOptions: {
+          mapTypeIds: [google.maps.MapTypeId.TERRAIN, MY_MAPTYPE_ID]
+        },
+        mapTypeId: MY_MAPTYPE_ID
+        };   
+          
+          
+// Calling the constructor, thereby initializing the map  
+          var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+          var styledMapOptions = { name: 'Torre de Hércules' };
+          var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+          map.mapTypes.set(MY_MAPTYPE_ID, customMapType); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var markers = [];         
+
+    var i, newMarker;
+
+   
+
+infowindow = new google.maps.InfoWindow();
+    
+
+    for (i = 0; i < sources.length; i++) {      
+      newMarker = new google.maps.Marker({        
+        position: new google.maps.LatLng(sources[i][1], sources[i][2]),        
+        map: map,  
+        shadow: sources[i][5],         
+        icon: sources[i][4],
+        info: sources[i][6],
+        title: sources[i][0],
+                clickable: true    
+              
+      });      
+
+      newMarker.category = sources[i][3];      
+      newMarker.setVisible(true);       
+      
+      markers.push(newMarker);  
+
+        google.maps.event.addListener(newMarker, 'click', function() {     
+        infowindow.setContent(this.info);    
+        infowindow.open(map,this);   
+
+       });
+      
+    }  
+
+
+function displayMarkers(category) {      
+      
+    var i;       
+
+      for (i = 0; i < sources.length; i++) {        
+        if (markers[i].category === category) { 
+            if (markers[i].visible === true){
+               markers[i].setVisible(false); }
+            else {
+               markers[i].setVisible(true);}         
+                   
+        }        
+      }    
+    }
+
+
+
+//    }
