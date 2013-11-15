8888f1cd05d441cc3fa724e2798813a5d218522f
Juanjo <juanjo@efimera.com>
2013-11-15 17:11:33 +0100

    Adds better timeline with centuries links

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -50,25 +50,70 @@ $( "#languages .showmenu" ).click(function() {
             
           });
 
-          $('.adslider').bxSlider({
-            
-            randomStart: true,
-            autoStart: true,
-            autoControls: false,
-            auto: true
-            
-          });
+          $('.timeline2 ul').bxSlider({
+              pagerType: 'full',
+              autoStart: true,
+              controls: false,
+              captions:false
+         });
 
-           $('.timeline ul').bxSlider({
-               pagerType: 'full',
-               autoStart: true,
-               controls: false,
-               captions:false
-          });
+           
 
-
-
-
+		  var slider = $('.timeline ul').bxSlider({
+		    mode: 'fade',
+			controls: false,
+			captions: false,
+			pager: false
+		  });
+		  $('#slider0').click(function(){
+		    slider.goToSlide(0);
+		    return false;
+		  });
+		  $('#slider1').click(function(){
+		    slider.goToSlide(1);
+		    return false;
+		  });
+		  $('#slider2').click(function(){
+		    slider.goToSlide(2);
+		    return false;
+		  });
+		  $('#slider3').click(function(){
+		    slider.goToSlide(3);
+		    return false;
+		  });
+		  $('#slider4').click(function(){
+		    slider.goToSlide(4);
+		    return false;
+		  });
+		  $('#slider5').click(function(){
+		    slider.goToSlide(5);
+		    return false;
+		  });
+		  $('#slider6').click(function(){
+		    slider.goToSlide(6);
+		    return false;
+		  });
+		  $('#slider7').click(function(){
+		    slider.goToSlide(7);
+		    return false;
+		  });
+		  $('#slider8').click(function(){
+		    slider.goToSlide(8);
+		    return false;
+		  });
+		  $('#slider9').click(function(){
+		    slider.goToSlide(9);
+		    return false;
+		  });
+		  $('#slider10').click(function(){
+		    slider.goToSlide(10);
+		    return false;
+		  });
+		  $('#slider11').click(function(){
+		    slider.goToSlide(11);
+		    return false;
+		  });
+		 
 
 
 // Share
